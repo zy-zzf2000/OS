b554445275970f85b2d110103d367dd4a270e60b
zy <953725892@qq.com>
2022-11-14 02:18:27 +0800

    初步完成所有基本功能的实现和测试

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
  * @Author: zy 953725892@qq.com
  * @Date: 2022-11-13 20:21:09
  * @LastEditors: zy 953725892@qq.com
- * @LastEditTime: 2022-11-14 00:19:51
+ * @LastEditTime: 2022-11-14 02:17:45
  * @FilePath: /lab1/main.c
  * @Description: 
  * 
@@ -14,7 +14,7 @@
 #include <unistd.h>
 #include <getopt.h>
 #include "string.h"
-
+#include <dirent.h>
 
 #define MAXLINE 1024
 
@@ -27,21 +27,22 @@
 int checkType(char* path){
     struct stat buf;
     fstatat(AT_FDCWD, path, &buf, 0);
-    //首先检查文件类型
-    if(S_ISREG(buf.st_mode)){
-        //检查文件权限
-        if(buf.st_mode & S_IRUSR){
-            printf("文件为普通文件，且有读权限\n");
+    //首先检查文件权限
+    if(buf.st_mode & S_IRUSR){
+        if(S_ISDIR(buf.st_mode)){
+            printf("文件为目录文件\n");
+            return 2;
+        }else if(S_ISREG(buf.st_mode)){
+            printf("文件为普通文件\n");
             return 1;
+        }else{
+            printf("文件类型错误\n");
+            return -1;
         }
-        else{
-            return -2;
-        }
-    }else if(S_ISDIR(buf.st_mode)){
-        printf("文件为目录文件\n");
-        return 2;
+    }else{
+        return -2;
     }
-    return -1;
+    
 }
 
 /**
@@ -50,27 +51,80 @@ int checkType(char* path){
  * @param {int} mode：是否忽略空行；1：忽略空行；0：不忽略空行
  * @return {*}：-1：文件打开错误；other：文件行数
  */
-int calLine(char* path,int mode){
+int calSingle(char* path,int mode){
     char* filepath = (char*) malloc(strlen(path)+strlen("./"));
     sprintf(filepath,"./%s",path);
-    FILE* fp = fopen("/home/zy/sourcecode/c/lab1/build/test.txt","r");
+    FILE* fp = fopen(filepath,"r");
     if(fp == NULL){
         printf("文件打开失败\n");
         return -1;
     }
     int line = 0;
-    char buf[MAXLINE];
-    while(fgets(buf,MAXLINE,fp)){
-        if(strcmp(buf,"\n")==0 && mode == 1){
-            continue;
+    if(mode == 1){
+        //如果忽略空行，用fgets读取文件
+        char buf[MAXLINE];
+        while(fgets(buf,MAXLINE,fp)!=NULL){
+            if(strcmp(buf,"\n")==0){
+                continue;
+            }
+            line++;
+        }
+    }else{
+        //如果不忽略空行，用fgetc读取文件
+        char buf;
+        while((buf = fgetc(fp))!=EOF){
+            if(buf == '\n'){
+                line++;
+            }
         }
         line++;
     }
-    //fgets会读取最后一个换行符，所以如果不忽略换行，需要加一
-    return mode==1?line:line+1;
+    printf("文件行数为：%d\n",line);
+    return line;
 }
 
-//TODO:完成命令行参数解析(Done)
+/**
+ * @description: 
+ * @param {char*} path：文件路径
+ * @param {int} mode：是否忽略空行；1：忽略空行；0：不忽略空行
+ * @param {int} recursive：是否递归处理子文件；1：递归；0：不递归
+ * @return {*}: -1：目录打开错误；other：文件行数
+ */
+int calDir(char* path,int mode,int recursive){
+    DIR *dir;
+    struct dirent *dp;
+    int lines = 0;
+
+    char* dirpath = (char*) malloc(strlen(path)+strlen("./"));
+    sprintf(dirpath,"./%s",path);
+    if((dir=opendir(dirpath))==NULL){
+        printf("目录打开失败\n");
+        return -1;
+    }
+    while((dp = readdir(dir)) != NULL){
+        //跳过.和..目录
+        if(strcmp(dp->d_name,".")==0 || strcmp(dp->d_name,"..")==0){
+            continue;
+        }
+        //构造子文件路径
+        char* filepath = (char*) malloc(strlen(path)+strlen(dp->d_name)+strlen("/"));
+        sprintf(filepath,"%s/%s",path,dp->d_name);
+        int type = checkType(filepath);
+        if (type == 1){
+            int singleline = calSingle(filepath,mode);
+            if(singleline>0){
+                lines += singleline;
+            }
+        }else if(type==2 && recursive==1){
+            int dirline = calDir(filepath,mode,recursive);
+            if(dirline>0){
+                lines += dirline;
+            }
+        }
+    }
+    return lines;
+}
+
 int main(int argc,char *argv[])
 {
     int options;
@@ -107,12 +161,14 @@ int main(int argc,char *argv[])
     }
 
     //输出剩余文件名
-    for(int i = optind; i < argc; i++){
-        printf("argv[%d] is %s\n", i, argv[i]);
-        int cal = calLine(argv[i],1);
-        printf("文件行数为：%d\n",cal);
-    }
+    // for(int i = optind; i < argc; i++){
+    //     printf("argv[%d] is %s\n", i, argv[i]);
+    //     int cal = calSingle(argv[i],1);
+    //     printf("文件行数为：%d\n",cal);
+    // }
 
+    int lines = calDir("test",0,1);
+    printf("文件夹行数为：%d\n",lines);
     return 0;
 }
 
